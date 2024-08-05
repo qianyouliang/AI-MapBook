d074915f1f1b8600ec58892f214f6521c20ec26c
qianyouliang <64001056+qianyouliang@users.noreply.github.com>
2024-08-05 15:07:50 +0800

    Update install.py
    # 完善了安装模型文件的代码

--- install.py
+++ install.py
@@ -6,11 +6,11 @@ from transformers import  AutoTokenizer
 
 if __name__ == '__main__':
     # 第一个参数表示下载模型的型号，第二个参数是下载后存放的缓存地址，第三个表示版本号，默认 master 配置llm
-    model_dir = snapshot_download('Qwen/Qwen2-7B', cache_dir='models', revision='master')
-    model_path = os.path.join(os.getcwd(),"models/Qwen/Qwen2-7B")
+    model_dir = snapshot_download('Qwen/Qwen2-7B-Instruct', cache_dir='models', revision='master')
+    model_path = os.path.join(os.getcwd(),"models/Qwen/Qwen2-7B-Instruct")
     model = AutoModelForCausalLM.from_pretrained(model_path, load_in_low_bit='sym_int4', trust_remote_code=True)
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    model.save_low_bit('qwen2chat_int4')
-    tokenizer.save_pretrained('qwen2chat_int4')
+    model.save_low_bit('models/qwen2chat_int4')
+    tokenizer.save_pretrained('models/qwen2chat_int4')
     # 配置embedding模型
     embedding_model_dir = snapshot_download('AI-ModelScope/bge-small-zh-v1.5', cache_dir='models', revision='master')
